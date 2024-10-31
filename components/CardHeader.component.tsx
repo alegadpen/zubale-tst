0b8a5887659482e809daef73703e54bfe3ca5617
Ale Gadpen <alegadpen@gmail.com>
2024-10-31 18:42:10 -0300

    Housekeeping, correct align location

--- components/CardHeader.component.tsx
+++ components/CardHeader.component.tsx
@@ -8,30 +8,31 @@ const Circle = () =>
     <View style={{ width: 20, height: 20, backgroundColor: 'red', borderRadius: 50, marginRight: 16 }}></View>
 
 export const CardHeader = ({ avatar, name, location } : HeaderData) => {
-    const [avatarError, setAvatarError] = useState(false);
     const [avatarLoaded, setAvatarLoaded] = useState(false);
 
     return (
-        <View style={generalStyles.padded}>
-            {avatarLoaded ?
-                <FastImage
-                    source={{ uri : avatar }}
-                    resizeMode={FastImage.resizeMode.contain}
-                    style={{
-                        width: 40,
-                        height: 40,
-                        borderRadius: 5,
-                    }}
-                    onLoad={() => setAvatarLoaded(true)}
-                    onError={() => setAvatarLoaded(false)}
-                /> :
-                <Circle />
-            }
-            <Text style={generalStyles.regularText}>{name}</Text>
-            <View>
-                <Text style={generalStyles.regularText}>{location}</Text>
+        <>
+            <View style={generalStyles.padded}>
+                {avatarLoaded ?
+                    <FastImage
+                        source={{ uri : avatar }}
+                        resizeMode={FastImage.resizeMode.contain}
+                        style={{
+                            width: 40,
+                            height: 40,
+                            borderRadius: 5,
+                        }}
+                        onLoad={() => setAvatarLoaded(true)}
+                        onError={() => setAvatarLoaded(false)}
+                    /> :
+                    <Circle />
+                }
+                <View>
+                <Text style={generalStyles.regularText}>{name}</Text>
+                    <Text style={generalStyles.regularText}>{location}</Text>
+                </View>
             </View>
-        </View>
+        </>
     );
 };
 
